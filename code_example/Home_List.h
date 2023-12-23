b46ea21d4033b11be74b488052801e92fdaf6e5b
陈懿轩 <152504264+Chen-Yixu-an@users.noreply.github.com>
2023-12-23 22:11:38 +0800

    First
    0.0.01

---
+++ code_example/Home_List.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<iostream>
+using namespace std;
+void use_way() {
+	cout << "input the code's name:" << endl;
+	cout << "input exit() to exit the application" << endl;
+}
+void input_() {
+	cout << "code_dic >";
+}
+
+void clear() {
+	system("cls");
+}
---
+++ code_example/Main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+#include"Code_Dic.h"
+#include"Home_List.h"
+using namespace std;
+
+int main()
+{
+	use_way();
+	string a;
+	while (true) {
+
+
+	}
+	return 0;
+}
